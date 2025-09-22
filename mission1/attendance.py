32bdb010b7aaa46dd47e8b7e0661ca0a5b3ba728
lee.janghee <lee.janghee@gmail.com>
2025-09-22 12:32:29 +0900

    refactoring method lever

--- mission1/attendance.py
+++ mission1/attendance.py
@@ -9,7 +9,7 @@ player_list = {}
 total_player = 0
 
 # dat[사용자ID][요일]
-dat = [[0] * 100 for _ in range(100)]
+attendance_record = [[0] * 100 for _ in range(100)]
 points = [0] * 100
 grade = [0] * 100
 names = [''] * 100
@@ -54,22 +54,54 @@ def calc_score(name, weekday):
 
     player_id = player_list[name]
 
-    index = POINT_RULE[weekday]['day']
-    dat[player_id][index] += 1
+    day = POINT_RULE[weekday]['day']
+    attendance_record[player_id][day] += 1
     points[player_id] += POINT_RULE[weekday]['point']
 
     if weekday == 'wednesday':
         attend_on_wednesday[player_id] += 1
-    elif weekday == 'saturday':
+    elif weekday == 'saturday' or weekday == 'sunday':
         attend_on_weekend[player_id] += 1
-    elif weekday == 'sunday':
-        attend_on_weekend[player_id] += 1
+
+
+def print_removed_player():
+    print("\nRemoved player")
+    print("==============")
+    for i in range(1, total_player + 1):
+        if grade[i] not in (1, 2) and attend_on_wednesday[i] == 0 and attend_on_weekend[i] == 0:
+            print(names[i])
+
+
+def display_info(player_id: int):
+    print(f"NAME : {names[player_id]}, POINT : {points[player_id]}, GRADE : ", end="")
+    if grade[player_id] == Grade.gold:
+        print("GOLD")
+    elif grade[player_id] == Grade.silver:
+        print("SILVER")
+    else:
+        print("NORMAL")
+
+
+def assign_grade(player_id):
+    if points[player_id] >= THRESHOLD_GOLD_GRADE:
+        grade[player_id] = Grade.gold
+    elif points[player_id] >= THRESHOLD_SILVER_GRADE:
+        grade[player_id] = Grade.silver
+    else:
+        grade[player_id] = Grade.normal
+
+
+def calc_bonus_point(player_id: int):
+    if attendance_record[player_id][Day.wednesday] > THRESHOLD_BONUS:
+        points[player_id] += BONUS_POINT
+    if attendance_record[player_id][Day.saturday] + attendance_record[player_id][Day.sunday] > THRESHOLD_BONUS:
+        points[player_id] += BONUS_POINT
 
 
 def manage_attendance():
     try:
         with open("attendance_weekday_500.txt", encoding='utf-8') as f:
-            for _ in range(500):
+            while True:
                 line = f.readline()
                 if not line:
                     break
@@ -77,32 +109,12 @@ def manage_attendance():
                 if len(file_input) == 2:
                     calc_score(file_input[0], file_input[1])
 
-        for i in range(1, total_player + 1):
-            if dat[i][Day.wednesday] > THRESHOLD_BONUS:
-                points[i] += BONUS_POINT
-            if dat[i][Day.saturday] + dat[i][Day.sunday] > THRESHOLD_BONUS:
-                points[i] += BONUS_POINT
+        for player_id in range(1, total_player + 1):
+            calc_bonus_point(player_id)
+            assign_grade(player_id)
+            display_info(player_id)
 
-            if points[i] >= THRESHOLD_GOLD_GRADE:
-                grade[i] = Grade.gold
-            elif points[i] >= THRESHOLD_SILVER_GRADE:
-                grade[i] = Grade.silver
-            else:
-                grade[i] = Grade.normal
-
-            print(f"NAME : {names[i]}, POINT : {points[i]}, GRADE : ", end="")
-            if grade[i] == Grade.gold:
-                print("GOLD")
-            elif grade[i] == Grade.silver:
-                print("SILVER")
-            else:
-                print("NORMAL")
-
-        print("\nRemoved player")
-        print("==============")
-        for i in range(1, total_player + 1):
-            if grade[i] not in (1, 2) and attend_on_wednesday[i] == 0 and attend_on_weekend[i] == 0:
-                print(names[i])
+        print_removed_player()
 
     except FileNotFoundError:
         print("파일을 찾을 수 없습니다.")
